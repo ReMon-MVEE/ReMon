a88832524f529dc9441fa7c9a393bd3cfdcbff00
Stijn Volckaert <stijnv@gmail.com>
2016-03-07 15:22:43 -0800

    * Use system-provided musl and explicitly request linking with the BFD linker

--- MVEE_LD_Loader/comp_amd64.sh
+++ MVEE_LD_Loader/comp_amd64.sh
@@ -1,2 +1,2 @@
-/usr/local/musl/bin/musl-gcc -std=c99 -o MVEE_LD_Loader -static MVEE_LD_Loader.c -T linkerscript_amd64.txt
+musl-gcc -fuse-ld=bfd -std=c99 -o MVEE_LD_Loader -static MVEE_LD_Loader.c -T linkerscript_amd64.txt
 cp MVEE_LD_Loader MVEE_LD_Loader_this_is_a_very_long_process_name_that_must_be_at_least_as_long_as_slash_lib64_slash_ld-linux-x86-64.so.2_times_two
