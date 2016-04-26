3a24a759db9f212d36b009bcce737d7d117e8491
Stijn Volckaert <stijnv@gmail.com>
2016-04-26 07:54:46 -0700

    fixed tonto runme script

--- MVEE/bin/Release/spec/465.tonto/ref/runme.sh
+++ MVEE/bin/Release/spec/465.tonto/ref/runme.sh
@@ -3,5 +3,7 @@ BINPATH=$BENCHPATH/build/$SPECPROFILE
 DATAPATH=$BENCHPATH/data/ref/input
 ALLDATAPATH=$BENCHPATH/data/all/input
 
+mkdir -p $MVEEROOT/MVEE/bin/Release/spec/mvee_run/465.tonto
 cd $MVEEROOT/MVEE/bin/Release/spec/mvee_run/465.tonto
+cp $DATAPATH/stdin .
 $BINPATH/tonto > tonto.out
