dd8533d85284fb7e3ca6f9c2f1392d3c9c503b8c
Stijn Volckaert <stijnv@gmail.com>
2016-04-26 07:26:07 -0700

    fixed runme script for dealII

--- MVEE/bin/Release/spec/447.dealII/ref/runme.sh
+++ MVEE/bin/Release/spec/447.dealII/ref/runme.sh
@@ -3,5 +3,6 @@ BINPATH=$BENCHPATH/build/$SPECPROFILE
 DATAPATH=$BENCHPATH/data/ref/input
 ALLDATAPATH=$BENCHPATH/data/all/input
 
+mkdir -p $MVEEROOT/MVEE/bin/Release/spec/mvee_run/447.dealII
 cd $MVEEROOT/MVEE/bin/Release/spec/mvee_run/447.dealII
 $BINPATH/dealII 23 > dealII.out
