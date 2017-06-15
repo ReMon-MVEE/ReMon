b2f023ca2da0ed08ad82d2db5625fe582e3dfc53
Stijn Volckaert <stijnv@gmail.com>
2017-06-15 15:08:49 -0700

    Small fix in handler table generation script

--- generate_syscall_tables.rb
+++ generate_syscall_tables.rb
@@ -37,12 +37,12 @@ File.open(@unistd).each { |line|
 }
 
 File.open("MVEE/Src/MVEE_syscalls_handlers.cpp").each { |line|
-  if line.match(/(GET_CALL_TYPE|PRECALL|LOG_ARGS|LOG_RETURN|POSTCALL|CALL)\([[:graph:]]*\)/)
+  if line.match(/^(GET_CALL_TYPE|PRECALL|LOG_ARGS|LOG_RETURN|POSTCALL|CALL)\([[:graph:]]*\)/)
     syscall     = line.split("(")[1].split(")")[0]
     handlertype = line.match(/(GET_CALL_TYPE|PRECALL|LOG_ARGS|LOG_RETURN|POSTCALL|CALL)/)[1]
     notneeded   = line.match(/DONTNEED/)
     handlerfunc = "handle_#{syscall}_#{handlertype.downcase}"
-    
+  
     @handlers[syscall] = Hash.new if not @handlers[syscall] 
     if notneeded
       @handlers[syscall][handlertype] = "MVEE_HANDLER_DONTNEED"
