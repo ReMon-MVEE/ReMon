66ecb9ca39e22e50a044d3a93ba24086bbf660fe
Stijn Volckaert <stijnv@gmail.com>
2017-07-03 16:12:50 -0700

    libc configuration script: set up ldconfig support properly

--- scripts/stijn-configure-libc.sh
+++ scripts/stijn-configure-libc.sh
@@ -1,1 +1,4 @@
+rm -rf $HOME/glibc-build/etc
+mkdir -p $HOME/glibc-build/etc
+cp /etc/ld.so.conf.d/x86_64-linux-gnu.conf $HOME/glibc-build/etc/ld.so.conf
 ../configure --host=x86_64-linux-gnu --build=x86_64-linux-gnu --prefix=/usr --enable-stackguard-randomization --enable-obsolete-rpc --enable-pt_chown --with-selinux --enable-lock-elision=no --enable-addons=nptl --prefix=$HOME/glibc-build
