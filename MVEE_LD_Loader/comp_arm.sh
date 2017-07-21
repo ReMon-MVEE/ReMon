07a60fef71a41a83289925bd6197d82b9238b78a
Stijn Volckaert <stijnv@gmail.com>
2017-07-21 00:14:44 +0000

    Added the necessary files to compile the LD Loader for ARM

--- MVEE_LD_Loader/comp_arm.sh
+++ MVEE_LD_Loader/comp_arm.sh
@@ -1,2 +1,3 @@
-echo "The MVEE LD Loader does not work on ARM yet"
-touch MVEE_LD_Loader
+../deps/musl-install/bin/musl-gcc -I./arm/ -marm -mthumb-interwork -fuse-ld=bfd -std=c99 -o MVEE_LD_Loader -static MVEE_LD_Loader.c -T linkerscript_arm.txt
+cp MVEE_LD_Loader MVEE_LD_Loader_this_is_a_very_long_process_name_that_must_be_at_least_as_long_as_slash_lib_slash_ld-linux-armhf.so.3_times_two
+chmod a+x MVEE_LD_Loader_this_is_a_very_long_process_name_that_must_be_at_least_as_long_as_slash_lib_slash_ld-linux-armhf.so.3_times_two
