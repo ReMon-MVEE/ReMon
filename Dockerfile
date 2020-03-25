f95c4c356787a563532c5a489b68e2c0931f6d65
babrath <bert.abrath@UGent.be>
2020-03-25 19:49:39 +0100

    Dockerization of the MVEE build dependencies.

---
+++ Dockerfile
@@ -0,0 +1,66 @@
+FROM ubuntu:18.04
+ARG DEBIAN_FRONTEND=noninteractive
+
+# Install the required packages
+RUN \
+    apt-get update && \
+    # Required to build dependencies
+    apt-get install -y gcc g++ cmake bison flex python texinfo texi2html automake zlib1g-dev \
+    # Required to build MVEE
+    ruby libselinux-dev musl-tools libelf-dev libdwarf-dev libgmp-dev libmpfr-dev libmpc-dev libconfig-dev libcap-dev libunwind8 libunwind8-dev liblzma5 liblzma-dev
+
+################################################################################################################################################################
+###################################################################### Build dependencies ######################################################################
+################################################################################################################################################################
+
+# Install binutils
+COPY deps/binutils /tmp/binutils/
+RUN \
+    mkdir -p /opt/deps/binutils/build-tree && \
+    cd /opt/deps/binutils/build-tree && \
+    /tmp/binutils/configure --enable-plugins --enable-gold --disable-werror && \
+    make -j `getconf _NPROCESSORS_ONLN`
+
+# Build musl
+COPY deps/musl /tmp/musl/
+RUN \
+    cd /tmp/musl && \
+    /tmp/musl/configure --prefix=/opt/deps/musl-install --exec-prefix=/opt/deps/musl-install && \
+    make -j `getconf _NPROCESSORS_ONLN` install
+
+# Build libjson
+COPY deps/jsoncpp /tmp/jsoncpp/
+RUN \
+    mkdir -p /opt/deps/jsoncpp/build && \
+    cd /opt/deps/jsoncpp/build && \
+    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS=-O3 \
+    -DCMAKE_INSTALL_INCLUDEDIR=include/jsoncpp -DCMAKE_INSTALL_PREFIX=/usr/ /tmp/jsoncpp && \
+    make -j `getconf _NPROCESSORS_ONLN` install
+
+# Install ReMon LLVM
+COPY deps/llvm /tmp/llvm/
+RUN \
+    mkdir -p /build/llvm/ && \
+    cd /build/llvm/ && \
+    cmake -DLLVM_TARGETS_TO_BUILD="X86;ARM" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/opt/deps/llvm/build-tree/ -DLLVM_BINUTILS_INCDIR=/tmp/binutils/include /tmp/llvm/ && \
+    make -j `getconf _NPROCESSORS_ONLN` install
+
+# Cleanup
+RUN rm -rf /tmp/*
+
+# Set environment variable in container, so when building MVEE we know all dependencies can be found in /usr
+ENV MVEE_DEPS_PRESENT_IN_SYSTEM yes
+
+################################################################################################################################################################
+########################################################################## Extra stuff #########################################################################
+################################################################################################################################################################
+
+# Install extra applications to run in MVEE, or tools to build those applications
+RUN \
+    apt-get update && \
+    apt-get install -y vim less strace
+
+# Make sure normal users have plenty of rights to /opt
+RUN chmod 777 /opt
+
+WORKDIR /projects
---
+++ docker_MVEE.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+# This script can be invoked in three modes:
+# - 'download'      => downloads the dependencies for the MVEE
+# - 'build'         => builds the dependencies in a Ubuntu 18.04 docker
+# - 'run' (default) => runs the docker, in which you can build and use the MVEE
+
+IMAGE=remon
+BUILD_DIR=$PWD/build
+SHARED_PROJECTS_DIR=$PWD/projects
+
+build_docker() {
+    if ! command -v docker &> /dev/null; then
+        echo "No docker installed!"
+        exit 1
+    fi
+
+    docker build . -t $IMAGE
+}
+
+download_deps() {
+    mkdir -p deps
+
+    # download binutils
+    if [ ! -e deps/binutils ]
+    then
+        wget ftp://sourceware.org/pub/binutils/snapshots/binutils-2.30.90.tar.xz
+        tar xJf binutils-2.30.90.tar.xz
+        mv binutils-2.30.90 deps/binutils
+        #	git clone git://sourceware.org/git/binutils-gdb.git deps/binutils
+        rm binutils-2.30.90.tar.xz
+    fi
+
+    # download remon llvm
+    if [ ! -e deps/llvm ]
+    then
+        git clone https://github.com/stijn-volckaert/remon-llvm.git deps/llvm
+    fi
+    if [ ! -e deps/llvm/tools/clang ]
+    then
+        git clone https://github.com/stijn-volckaert/remon-clang.git deps/llvm/tools/clang
+    fi
+    if [ ! -e deps/llvm/projects/compiler-rt ]
+    then
+        git clone https://github.com/stijn-volckaert/remon-compiler-rt.git deps/llvm/projects/compiler-rt
+    fi
+
+    # download libjson
+    if [ ! -e deps/jsoncpp ]
+    then
+        git clone https://github.com/open-source-parsers/jsoncpp.git deps/jsoncpp
+        cd deps/jsoncpp
+        git reset --hard 6a15ca64429e1ce6d2fff915ff14ce1c2a96975c
+        cd -
+    fi
+
+    # download musl
+    if [ ! -e deps/musl ]
+    then
+        wget http://www.musl-libc.org/releases/musl-1.1.19.tar.gz
+        tar xzf musl-1.1.19.tar.gz
+        mv musl-1.1.19 deps/musl
+        rm musl-1.1.19.tar.gz
+    fi
+}
+
+run_docker() {
+    if ! command -v x11docker &> /dev/null; then
+        echo "No x11docker installed! Download at https://github.com/mviereck/x11docker"
+        exit 1
+    fi
+
+    # This script adds numerous volumes to the container:
+    # - the repository, containing all the code
+    # - a persistent home folder. This is provided by x11docker, and can contain your config files and bash history (across docker runs!)
+    # - a shared 'projects' folder, where you can place applications to build and/or run in the MVEE, as well as their data.
+    # - the 'build' data volume. This named volume can be used to incrementally build LLVM (or other applications) in.
+
+    # Make these folders here. If docker makes them for us, they will be root-owned...
+    mkdir -p $BUILD_DIR
+    mkdir -p $SHARED_PROJECTS_DIR
+
+    # The following command consists of:
+    # 1st line: the x11docker invocation and its options
+    # 2nd line: the docker options (mounting the repo, shared projects folder, and the build named data volume)
+    # 3rd line: the actual docker image and the command to run in it
+    x11docker --gpu --pulseaudio --interactive --home -- \
+        --cap-add SYS_PTRACE -ti --volume  "$PWD:/opt/repo" --volume "$SHARED_PROJECTS_DIR:/projects" --volume "build:/build" -- \
+        $IMAGE bash
+}
+
+# Check the number of parameters
+if [ "$#" -ne 1 ]; then
+    echo "No mode specified, trying to just run the docker."
+    mode="run"
+else
+    mode="$1"
+fi
+
+case "$mode" in
+    build)
+        build_docker
+        ;;
+
+    download)
+        download_deps
+        ;;
+
+    run)
+        run_docker
+        ;;
+
+    *)
+        echo "Invalid mode specified!"
+        exit 2
+        ;;
+esac
