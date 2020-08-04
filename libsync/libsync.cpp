980c922a4005470d3717dfc4e85a079c01ad2cdb
babrath <bert.abrath@UGent.be>
2020-08-04 17:18:57 +0200

    Adding mvee_shm_op_trampoline to libsync.

--- libsync/libsync.cpp
+++ libsync/libsync.cpp
@@ -1,5 +1,11 @@
+struct mvee_shm_op_ret {
+  unsigned long val;
+  bool cmp;
+};
+
 extern "C" unsigned char mvee_atomic_preop(unsigned char, void*);
 extern "C" void mvee_atomic_postop(unsigned char);
+extern "C" mvee_shm_op_ret mvee_shm_op(unsigned char id, bool atomic, void* address, unsigned long size, unsigned long value, unsigned long cmp);
 
 extern "C" unsigned char mvee_atomic_preop_trampoline(unsigned char type, void* variable)
 {
@@ -10,4 +16,9 @@ extern "C" unsigned char mvee_atomic_preop_trampoline(unsigned char type, void* 
 extern "C" void mvee_atomic_postop_trampoline(unsigned char preop_result)
 {
 	mvee_atomic_postop(preop_result);
+}
+
+extern "C" mvee_shm_op_ret mvee_shm_op_trampoline(unsigned char id, bool atomic, void* address, unsigned long size, unsigned long value, unsigned long cmp)
+{
+  return mvee_shm_op(id, atomic, address, size, value, cmp);
 }
