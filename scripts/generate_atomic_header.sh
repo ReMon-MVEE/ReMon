9dc6f9b917f1dba525ccf52478b5e2fdf55859c5
babrath <bert.abrath@UGent.be>
2021-05-04 10:04:36 +0200

    Renamed script, removed old version.

--- scripts/generate_atomic_header.sh
+++ scripts/generate_atomic_header.sh
@@ -1,6 +1,6 @@
 # fetch latest atomics definitions from gnu webpage
-lynx -width=1024 -dump "http://gcc.gnu.org/onlinedocs/gcc/_005f_005fatomic-Builtins.html" > atomics.txt
-lynx -width=1024 -dump "http://gcc.gnu.org/onlinedocs/gcc-4.4.5/gcc/Atomic-Builtins.html" > sync.txt
+lynx -width=1024 -dump "http://gcc.gnu.org/onlinedocs/gcc/_005f_005fatomic-Builtins.html" | grep ptr | egrep -v 'atomic_is_lock_free|atomic_always_lock_free' > atomics.txt
+lynx -width=1024 -dump "http://gcc.gnu.org/onlinedocs/gcc-4.4.5/gcc/Atomic-Builtins.html" | grep ptr > sync.txt
 
 # generate atomic ops enum
 echo "/* supported atomic ops */"
@@ -9,7 +9,7 @@ grep "\_\_atomic.*(" atomics.txt | cut -d':' -f2 | sed 's/  */ /g' | cut -d' ' -
 do
     echo "  mvee$line,"
 done
-grep "\_\_sync.*(" sync.txt | grep -v "    " | sed 's/\_\_sync\_synchronize/void \_\_sync\_synchronize/' | sed 's/  */ /g' | cut -d' ' -f3 | sed 's/\_\_/\_/' | while read line;
+grep "\_\_sync.*(" sync.txt | grep -v "    " | sed 's/  */ /g' | cut -d' ' -f3 | sed 's/\_\_/\_/' | while read line;
 do
     echo "  mvee$line,"
 done
@@ -22,8 +22,8 @@ echo "/* interceptable functions */"
 echo "#ifdef __cplusplus"
 echo "extern \"C\" {"
 echo "#endif"
-echo "void mvee_atomic_preop(unsigned short op, void* word_ptr);"
-echo "void mvee_atomic_postop();"
+echo "extern unsigned char __attribute__((weak)) mvee_atomic_preop(unsigned short op, void* word_ptr) { return 0; }"
+echo "extern void __attribute__((weak)) mvee_atomic_postop(unsigned char __preop_result) {}"
 echo "#ifdef __cplusplus"
 echo "}"
 echo "#endif"
@@ -32,7 +32,7 @@ echo " "
 # generate call macros to original intrinsics
 echo "/* call macros to the original intrinsics */"
 grep "\_\_atomic.*(" atomics.txt | cut -d':' -f2 | sed 's/type\|bool\|void\|int\|\*\|size\_t//g' | sed 's/  / /g' | sed 's/ \_\_\(.*\)/#define orig\_\1 \_\_\1/' | sed 's/ (/(/g' | sed 's/( /(/g'
-grep "\_\_sync.*(" sync.txt | grep -v "    " | sed 's/\_\_sync\_synchronize/void \_\_sync\_synchronize/' | sed 's/type oldval type newval/type oldval, type newval/' | sed 's/  */ /g' | sed 's/type\|bool\|void\|int\|\*\|size\_t//g' | sed 's/  \_\_\(.*\)/#define orig\_\1 \_\_\1/' | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/ (/(/g' | sed 's/( /(/g'
+grep "\_\_sync.*(" sync.txt | grep -v "    " | sed 's/type oldval type newval/type oldval, type newval/' | sed 's/  */ /g' | sed 's/type\|bool\|void\|int\|\*\|size\_t//g' | sed 's/  \_\_\(.*\)/#define orig\_\1 \_\_\1/' | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/ (/(/g' | sed 's/( /(/g'
 echo " "
 
 # generate mvee wrappers
@@ -48,16 +48,16 @@ do
 
     if [ "$func_type" == "void" ]
     then
-	echo "#define $stripped_func ({ mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); orig$stripped_func_2; mvee_atomic_postop(); })" | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
+	echo "#define $stripped_func ({ unsigned char __preop_result = mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); orig$stripped_func_2; mvee_atomic_postop(__preop_result); })" | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
     elif [ "$func_type" == "type" ]
     then
-	echo "#define $stripped_func ({ typeof (*ptr + 0) __ret; mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(); __ret; })" | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
+	echo "#define $stripped_func ({ typeof (*ptr + 0) __ret; unsigned char __preop_result = mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(__preop_result); __ret; })" | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
     else
-	echo "#define $stripped_func ({ $func_type __ret; mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(); __ret; })" | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
+	echo "#define $stripped_func ({ $func_type __ret; unsigned char __preop_result = mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(__preop_result); __ret; })" | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
     fi
 done
 
-grep "\_\_sync.*(" sync.txt | grep -v "    " | sed 's/\_\_sync\_synchronize/void \_\_sync\_synchronize/' | sed 's/type oldval type newval/type oldval, type newval/' | while read line;
+grep "\_\_sync.*(" sync.txt | grep -v "    " | sed 's/type oldval type newval/type oldval, type newval/' | while read line;
 do
     func=$line
     stripped_func=`echo $func | sed 's/type\|bool\|void\|int\|\*\|size\_t//g' | sed 's/  / /g'`
@@ -68,12 +68,12 @@ do
 
     if [ "$func_type" == "void" ]
     then
-        echo "#define $stripped_func ({ mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); orig$stripped_func_2; mvee_atomic_postop(); })" | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
+        echo "#define $stripped_func ({ unsigned char __preop_result = mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); orig$stripped_func_2; mvee_atomic_postop(__preop_result); })" | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
     elif [ "$func_type" == "type" ]
     then
-        echo "#define $stripped_func ({ typeof (*ptr + 0) __ret; mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(); __ret; })" | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
+        echo "#define $stripped_func ({ typeof (*ptr + 0) __ret; unsigned char __preop_result = mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(__preop_result); __ret; })" | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
     else
-        echo "#define $stripped_func ({ $func_type __ret; mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(); __ret; })" | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
+        echo "#define $stripped_func ({ $func_type __ret; unsigned char __preop_result = mvee_atomic_preop(mvee$func_name_2, (void*)(unsigned long)ptr); __ret = orig$stripped_func_2; mvee_atomic_postop(__preop_result); __ret; })" | sed 's/\(.*\)\.\.\.\(.*\)/\1##\_\_VA\_ARGS\_\_\2/' | sed 's/( /(/g' | sed 's/ (/(/g' | sed 's/)(/) (/g'
     fi
 done
 
