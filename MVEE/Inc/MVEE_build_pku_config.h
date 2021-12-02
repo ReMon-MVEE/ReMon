24de70a74d6c7172a4bb38c6bb277e6fe3017e10
Alexios Voulimeneas <balexios@gmail.com>
2021-12-02 18:45:30 +0100

    Update

--- MVEE/Inc/MVEE_build_pku_config.h
+++ MVEE/Inc/MVEE_build_pku_config.h
@@ -8,7 +8,7 @@
 //
 // MVEE_IP_PKU_ENABLED: If this is defined IP-MON uses PKU.
 // If changed both IP-MON and GHUMVEE should be recompiled.
-//
+// !!! This requires a kernel with ipmon-pku-assisted support (check linux-5.4.0-full-ipmon-pku-assisted.patch)
 // #define MVEE_IP_PKU_ENABLED
 
 #endif /* MVEE_BUILD_PKU_CONFIG_H_ */
