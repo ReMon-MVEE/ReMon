d56aff0e733e14ef7978316b5b79a1f55707dbcd
Bart Coppens <Bart.Coppens@UGent.be>
2022-02-01 22:44:58 +0100

    Copy over MVEE_backtrace from private repo's old state

--- Utilities/MVEE_backtrace/MVEE_backtrace.c
+++ Utilities/MVEE_backtrace/MVEE_backtrace.c
@@ -50,34 +50,6 @@ char* mvee_log_read_from_proc_pipe(const char* proc, int* output_length)
 
 int  main(int argc, char** argv)
 {
-    long  fd       = syscall(__NR_open, "/tmp/MVEE_signal_file.tmp", O_RDWR);
-
-    if (fd < 0 && fd > -4096)
-    {
-        printf("couldn't open signal file!!!\n");
-        return -1;
-    }
-    printf("signal file opened: %ld\n", fd);
-
-    char* read_buf = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-    syscall(__NR_close, fd);
-
-    if (read_buf == MAP_FAILED)
-    {
-        printf("mmap error: %ld (%s)\n", errno, strerror(errno));
-        return -1;
-    }
-    else
-    {
-        printf("read buf mapped\n");
-        read_buf[0]='1';
-        printf("read buf written\n");
-    }
-
-    munmap(read_buf, 4096);
-
-    printf("Signaled the monitor...\n");
-
     char* pid = mvee_log_read_from_proc_pipe("ps ux | grep \"\\MVEE \" | grep -v grep | sed -e 's/  */ /g' | cut -d' ' -f2", NULL);
 
     int   i;
@@ -93,7 +65,7 @@ int  main(int argc, char** argv)
         //sprintf(cmd, "kill -int %d\n", _pid);
         //      sprintf(cmd, "kill -quit %d\n", _pid);
         //      mvee_log_read_from_proc_pipe(cmd, NULL);
-        syscall(__NR_tgkill, _pid, _pid, SIGQUIT);
+		syscall(__NR_tgkill, _pid, _pid, SIGUSR2);
     }
 
     return 0;
